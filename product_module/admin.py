4226bb631674a83b08a3685c1ad3002c2f67f888
amiram0011 <mojtabagafarzade@gmail.com>
2023-04-11 17:31:21 +0330

    new table in db

--- product_module/admin.py
+++ product_module/admin.py
@@ -8,8 +8,10 @@ class productAdmin(admin.ModelAdmin):
         'slug': ['title']
     }
     list_filter = ['title', 'prise', 'is_active']
-    list_display = ['title','prise', 'is_active']
+    list_display = ['title', 'prise', 'is_active']
     list_editable = ['is_active']
 
 
 admin.site.register(models.product, productAdmin)
+admin.site.register(models.productCategory)
+admin.site.register(models.productinformation)
--- product_module/models.py
+++ product_module/models.py
@@ -8,9 +8,32 @@ class productCategory(models.Model):
     title = models.CharField(max_length=300, verbose_name='عنوان')
     url_title = models.CharField(max_length=300, verbose_name='عنوان در url')
 
+    def __str__(self):
+        return f'{self.title}:{self.url_title}'
+
+    class Meta:
+        verbose_name = 'دسته بندی'
+        verbose_name_plural = 'دسته بندی  ها'
+
+
+class productinformation(models.Model):
+    color = models.CharField(max_length=200, verbose_name='رنگ')
+    size = models.CharField(max_length=200, verbose_name='سایز')
+
+    def __str__(self):
+        return f'({self.color}:{self.size})'
+
+    class Meta:
+        verbose_name = 'اطلاعات تکمیلی'
+        verbose_name_plural = 'تمامی اطلاعات تکمیلی'
+
 
 class product(models.Model):
-    category = models.ForeignKey(productCategory, on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey(productCategory, on_delete=models.CASCADE, null=True, verbose_name='دسته بندی',
+                                 related_name='product')
+    product_information = models.OneToOneField('productinformation', on_delete=models.CASCADE,
+                                               related_name='product_information', verbose_name='اطلاعات تکمیلی',
+                                               null=True, blank=True)
     title = models.CharField(max_length=300)
     prise = models.IntegerField()
     rating = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)], default=0)
@@ -27,3 +50,7 @@ class product(models.Model):
 
     def __str__(self):
         return f'{self.title}:{self.prise}'
+
+    class Meta:
+        verbose_name = 'محصول'
+        verbose_name_plural = 'محصولات'
